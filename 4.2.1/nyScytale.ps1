8d76ae5e6617b5f5ec7799d1d07aad468f340d8f
Kristoffer SLettebakken <krissl@student.uia.no>
2019-09-25 22:33:43 +0200

    scytale add

---
+++ 4.2.1/nyScytale.ps1
@@ -0,0 +1,29 @@
+﻿#DENNE FUNKER BOYYYY
+function decrypt(){
+$input  = Read-host "Input message to decrypt"
+$key = Read-host "Key number"
+$rows = $input.Length/$key
+$decrypted = ""
+
+
+for($i=0;$i -lt $rows; $i++){
+for ($o=0; $o -lt $key; $o++){
+$decrypted += $input[$i+$o*$rows]
+}#end for
+}#end for 
+echo $decrypted
+}#end decrypt 
+
+function encrypt(){
+$input  = Read-host "Input message to decrypt"
+$key = Read-host "Key number"
+$rows = $input.Length/$key
+$encrypted = ""
+
+for ($i=0; $i -lt $key;$i++){
+for ($o=0; $o -lt $rows; $o++){
+$encrypted += $input[$i + $o *$key]
+}#for end
+}#for end
+echo $encrypted
+}#end encrypt 
